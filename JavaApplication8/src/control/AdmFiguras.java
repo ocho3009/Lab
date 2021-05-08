856b133f95783be8c40829ad02f9fc8b1513c7e2
ocho3009 <ocho3009@gmail.com>
2021-05-08 14:41:41 -0600

    cambio

--- JavaApplication8/src/control/AdmFiguras.java
+++ JavaApplication8/src/control/AdmFiguras.java
@@ -10,12 +10,5 @@ package control;
  * @author Chris
  */
 public class AdmFiguras {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
