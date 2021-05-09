45d48e1ea41deae235543217d3a7fc0f595de796
ocho3009 <ocho3009@gmail.com>
2021-05-09 02:00:34 -0600

    otra vez

--- JavaApplication8/src/control/Figura.java
+++ JavaApplication8/src/control/Figura.java
@@ -9,6 +9,61 @@ package control;
  *
  * @author Chris
  */
-public class Figura {
+public abstract class Figura {
+    private double area;
+    private boolean tipoFigura;
+    private int nombreFigura;
+    private int colores;
+    private double perimetro;
+
+    public Figura(boolean tipoFigura, int nombreFigura, int color) {
+        this.tipoFigura = tipoFigura;
+        this.nombreFigura = nombreFigura;
+        this.colores = colores;
+    }
+
+    public Figura() {
+        /* contructor generico*/
+    }
+    
+
+    public abstract double getArea();
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public abstract double getPerimetro();
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public boolean isTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(boolean tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    public int getNombreFigura() {
+        return nombreFigura;
+    }
+
+    public void setNombreFigura(int nombreFigura) {
+        this.nombreFigura = nombreFigura;
+    }
+
+    public int getColor() {
+        return colores;
+    }
+
+    public void setColor(int colores) {
+        this.colores = colores;
+    }
+    
+    
     
 }
+
